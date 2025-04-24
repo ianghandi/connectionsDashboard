f45027aa1839e2f06f9f1f7f2d25fe6a3018f8f2
ianghandi <ianghandi@gmail.com>
2025-04-24 11:42:39 -0400

    Adding frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyIntegrations from "./components/MyIntegrations";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white dark:bg-black transition-colors duration-300">
+      <MyIntegrations />
+    </div>
+  );
+};
+
+export default App;
